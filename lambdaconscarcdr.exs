9824d3b7ea27dcdfb7aa5641a3b9d161e00b5aec
Jörg W Mittag <JoergWMittag@GoogleMail.Com>
2017-05-11 02:12:30 +0200

    Use the capture operator with placeholders for anonymous functions in Elixir.

--- lambdaconscarcdr.exs
+++ lambdaconscarcdr.exs
@@ -1,8 +1,8 @@
 #!/usr/bin/env elixir
 
-kons  = fn hd, tl -> fn x -> if x do hd else tl end end end
-virst = fn l -> l.(true ) end
-rrest = fn l -> l.(false) end
+kons  = fn hd, tl -> &(if &1 do hd else tl end) end
+virst = &(&1.(true ))
+rrest = &(&1.(false))
 
 lstt  = kons.(1, kons.(2, nil))
 
